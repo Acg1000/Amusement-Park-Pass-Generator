3054b69fdc74714cf72f72bb1eede48b7b82fd39
andrewgraves(MAC) <andrewcgraves@gmail.com>
2019-07-03 08:59:49 -0700

    Added the last bits for real this time

--- Amusement Park Pass Generator (Part 1) - TD PROJECT 4/ViewController.swift
+++ Amusement Park Pass Generator (Part 1) - TD PROJECT 4/ViewController.swift
@@ -18,8 +18,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         var employee: Employee?
+        var employeeFailure: Employee?
         var guest: Guest?
+        var guestFailure: Guest?
+        var manager: Manager?
 
+        
+        // A normal guest creation instance
         do {
             guest = try create.guest(firstName: "Andrew", lastName: "Graves", age: nil, isVIP: true)
         } catch invalidInformationError.missingCredential(let missingInformation) {
@@ -34,8 +39,23 @@ class ViewController: UIViewController {
             fatalError()
         }
         
+        // Fails because a guest is created to have a child pass but it too old
+        do {
+            guestFailure = try create.guest(firstName: "Andrew", lastName: "Graves", age: 8, isVIP: false)
+        } catch invalidInformationError.missingCredential(let missingInformation) {
+            print("Oops, looks like you forgot to input a \(missingInformation)")
+            // Pop up an alert dialog
+            
+        } catch invalidInformationError.invalidAge {
+            print("You are too old to obtain a child pass!")
+            // Pop up an alert dialog
+            
+        } catch {
+            fatalError()
+        }
         
         
+        // Normal employee pass
         do {
             employee = try create.employee(firstName: "Andrew", lastName: "Graves", streetAddress: "123 fake drive", city: "Fake Valley", state: "California", zipCode: 111111, type: .maintenance)
         } catch invalidInformationError.missingCredential(let missingInformation) {
@@ -45,14 +65,40 @@ class ViewController: UIViewController {
             fatalError()
         }
         
-
         
+        // Fails because a street address is missing
+        do {
+            employeeFailure = try create.employee(firstName: "Andrew", lastName: "Graves", streetAddress: "", city: "Fake Valley", state: "California", zipCode: 111111, type: .maintenance)
+        } catch invalidInformationError.missingCredential(let missingInformation) {
+            print("Oops, looks like you forgot to input a \(missingInformation)")
+            // Pop up an alert dialog
+        } catch {
+            fatalError()
+        }
+        
+        
+        // Regular manager creation
+        do {
+            manager = try create.manager(firstName: "Andrew", lastName: "Graves", streetAddress: "123 fake drive", city: "Fake Valley", state: "California", zipCode: 111111)
+        } catch invalidInformationError.missingCredential(let missingInformation) {
+            print("Oops, looks like you forgot to input a \(missingInformation)")
+            // Pop up an alert dialog
+        } catch {
+            fatalError()
+        }
+        
+
+        // Call all the swipe methods here
         if let guest = guest {
             guest.swipe()
         }
         
         if let employee = employee {
             employee.swipe()
+        }
+        
+        if let manager = manager {
+            manager.swipe()
         }
         
     }
